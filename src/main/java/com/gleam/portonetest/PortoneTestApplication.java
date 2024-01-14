23365ab966f133f6682a39ce19e6f22c736e11d0
marooo326 <ktnqkr01@gmail.com>
2024-01-14 17:32:29 +0900

    inital commit

---
+++ src/main/java/com/gleam/portonetest/PortoneTestApplication.java
@@ -0,0 +1,13 @@
+package com.gleam.portonetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortoneTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortoneTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gleam/portonetest/PortoneTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gleam.portonetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortoneTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
